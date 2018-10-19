b4e1a710fbba4abb960cfdcff078b504e8f8e07f
Hoang Long <litpuvn@gmail.com>
2018-10-18 23:00:13 -0500

    plot raw data

---
+++ baselines/raw_data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+samples = 8352
+
+
+df = pd.read_csv('ANDR1602_clean.csv', sep=',')
+df = df.drop(columns=["id", "time step"])
+features = ["wind direction", "temperature", "humidity", "pressure",
+            "dewpoint", "wind speed at 2 meters", "solar radiation"]
+target = ["wind speed"]
+
+X = [i for i in range(samples)]
+y = df[target]
+
+# plt.scatter(X_test, y_test_true, color='red')
+# plt.plot(idx_test, y_test_true, color='blue')
+plt.plot(X, y, color='pink')
+plt.show()
+
