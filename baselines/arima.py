55ab2fa1b937a04848cc08421693098412d87046
Hoang Long <litpuvn@gmail.com>
2018-10-18 22:55:05 -0500

    add arima

--- baselines/arima.py
+++ baselines/arima.py
@@ -6,33 +6,47 @@ from matplotlib import pyplot
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import math
 import numpy as np
-
-def parser(x):
-    return datetime.strptime( x, '%Y-%m')
+import pandas as pd
 
 
-series = read_csv('carson-training.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
-series = series.reindex(index=series.index[::-1])
+df = pd.read_csv('ANDR1602_clean.csv', sep=',')
+df = df.drop(columns=["id", "time step"])
 
-# # fit model
-# model = ARIMA(series, order=(5, 1, 0))
-# model_fit = model.fit(disp=0)
-# print(model_fit.summary())
-# # plot residual errors
-# residuals = DataFrame(model_fit.resid)
-# residuals.plot()
-# pyplot.show()
-# residuals.plot(kind='kde')
-# pyplot.show()
-# print(residuals.describe())
+features = ["wind direction", "temperature", "humidity", "pressure",
+            "dewpoint", "wind speed at 2 meters", "solar radiation"]
+target = ["wind speed"]
 
+
+series = df[target]
+
+
+# fit model
+
+
+#
+# # # plot residual errors
+# # residuals = DataFrame(model_fit.resid)
+# # residuals.plot()
+# # pyplot.show()
+# # residuals.plot(kind='kde')
+# # pyplot.show()
+# # print(residuals.describe())
+#
 X = series.values
-size = int(len(X) * 0.66)
+size = int(len(X) * 0.9)
 # size = 50
 train, test = X[0:size], X[size:len(X)]
 history = [x for x in train]
-predictions = list()
 
+# SIMPLE ARIMA MODEL
+# model = ARIMA(history, order=(5, 1, 0))
+# model_fit = model.fit(disp=0)
+# print(model_fit.summary())
+# predictions, stderr, conf_int = model_fit.forecast(steps=len(test))
+
+
+## CONTINUOS TRAINING MODEL
+predictions = list()
 for t in range(len(test)):
     model = ARIMA(history, order=(5,1,0))
     model_fit = model.fit(disp=0)
@@ -42,7 +56,7 @@ for t in range(len(test)):
         predictions.append([yhat_i])
     obs = test[t]
     history.append(obs)
-    print('predicted=%f, expected=%f' % (yhat, obs))
+    # print('predicted=%f, expected=%f' % (yhat, obs))
     # break
 
 
@@ -56,10 +70,10 @@ print("Root Mean Squared Error: " + str(math.sqrt(mse)))
 
 r2 = r2_score(test, predictions)
 print("R Squared Error: " + str(r2))
-
-# plot
-pyplot.scatter(np.arange(0, len(test), 1), test, color='red')
-pyplot.plot(test, color='blue')
-pyplot.plot(predictions, color='pink')
-pyplot.show()
-
+#
+# # plot
+# pyplot.scatter(np.arange(0, len(test), 1), test, color='red')
+# pyplot.plot(test, color='blue')
+# pyplot.plot(predictions, color='pink')
+# pyplot.show()
+#
