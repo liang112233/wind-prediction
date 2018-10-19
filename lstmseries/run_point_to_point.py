b47e0efce63d0cac324e6e6f2aa0c7b0bbcb3bc6
Hoang Long <litpuvn@gmail.com>
2018-10-19 13:35:34 -0500

    using in memory training

--- lstmseries/run_point_to_point.py
+++ lstmseries/run_point_to_point.py
@@ -50,29 +50,29 @@ def main():
         normalise=configs['data']['normalise']
     )
 
-    '''
-	# in-memory training
-	model.train(
+
+    # in-memory training
+    model.train(
 		x,
 		y,
 		epochs = configs['training']['epochs'],
 		batch_size = configs['training']['batch_size'],
 		save_dir = configs['model']['save_dir']
 	)
-	'''
+
     # out-of memory generative training
-    steps_per_epoch = math.ceil((data.len_train - configs['data']['sequence_length']) / configs['training']['batch_size'])
-    model.train_generator(
-        data_gen=data.generate_train_batch(
-            seq_len=configs['data']['sequence_length'],
-            batch_size=configs['training']['batch_size'],
-            normalise=configs['data']['normalise']
-        ),
-        epochs=configs['training']['epochs'],
-        batch_size=configs['training']['batch_size'],
-        steps_per_epoch=steps_per_epoch,
-        save_dir=configs['model']['save_dir']
-    )
+    # steps_per_epoch = math.ceil((data.len_train - configs['data']['sequence_length']) / configs['training']['batch_size'])
+    # model.train_generator(
+    #     data_gen=data.generate_train_batch(
+    #         seq_len=configs['data']['sequence_length'],
+    #         batch_size=configs['training']['batch_size'],
+    #         normalise=configs['data']['normalise']
+    #     ),
+    #     epochs=configs['training']['epochs'],
+    #     batch_size=configs['training']['batch_size'],
+    #     steps_per_epoch=steps_per_epoch,
+    #     save_dir=configs['model']['save_dir']
+    # )
 
     x_test, y_test = data.get_test_data(
         seq_len=configs['data']['sequence_length'],
